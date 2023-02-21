2e0767a5863a3e3088b1b7db8ccd5e5f7febea5b
SubhadeepBid <subhadipbid@gmail.com>
2023-02-21 11:40:58 +0530

    MainApplication and Database properties files Added

---
+++ Controller/src/main/java/com/niccopark/NiccoParkApplication.java
@@ -0,0 +1,13 @@
+package com.niccopark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiccoParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiccoParkApplication.class, args);
+	}
+
+}
