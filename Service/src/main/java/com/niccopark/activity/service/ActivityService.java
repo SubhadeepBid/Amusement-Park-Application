b5ad9958c7b3a8a4814f1c049463e981e1def2cd
kanhaiya1007 <92618657+kanhaiya1007@users.noreply.github.com>
2023-02-22 09:10:18 -0800

    add .

---
+++ Service/src/main/java/com/niccopark/activity/service/ActivityService.java
@@ -0,0 +1,22 @@
+package com.niccopark.activity.service;
+
+import java.util.List;
+
+import javax.security.auth.login.LoginException;
+
+import com.niccopark.entity.Activity;
+import com.niccopark.exceptions.ActivityException;
+
+public interface ActivityService  {
+
+	
+	public Activity insertActivity(Activity activity) throws ActivityException;
+
+	public Activity updateActivity(Activity activity) throws ActivityException;
+
+	public Activity deleteActivity(int activityid) throws ActivityException;
+
+	public List<Activity> viewActivityofCharges(float charges) throws ActivityException;
+
+	public int countActivityofCharges(float charges) throws ActivityException;
+}
