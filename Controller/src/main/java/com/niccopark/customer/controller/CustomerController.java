8b34aeb9f9da36ec03d117191731d578d5051364
Shiv-96 <kshiv.dot@gmail.com>
2023-02-22 20:56:19 +0530

    Customer controller updated

--- Controller/src/main/java/com/niccopark/customer/controller/CustomerController.java
+++ Controller/src/main/java/com/niccopark/customer/controller/CustomerController.java
@@ -4,14 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.niccopark.customer.service.CustomerService;
 import com.niccopark.entity.Customer;
-import com.niccopark.exceptions.CustomerException;
-
-import jakarta.validation.Valid;
 
 @RestController
 //@RequestMapping(value = "/Customer")
