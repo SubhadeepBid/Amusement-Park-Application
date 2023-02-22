902055be2977201cd74ad53fe741c57eaef0c5dd
SubhadeepBid <subhadipbid@gmail.com>
2023-02-22 16:09:29 +0530

    Updates from Entity, Repository, Service Modules

--- Service/src/main/java/com/niccopark/admin/service/AdminService.java
+++ Service/src/main/java/com/niccopark/admin/service/AdminService.java
@@ -1,5 +1,7 @@
 package com.niccopark.admin.service;
 
+import com.niccopark.dtos.UpdateUserPasswordDTO;
+import com.niccopark.dtos.UpdateUserUsernameDTO;
 import com.niccopark.dtos.ValidateUserDTO;
 import com.niccopark.entity.Admin;
 import com.niccopark.exceptions.AdminException;
@@ -10,13 +12,13 @@ public interface AdminService {
 	
 	public Admin validateAdmin(ValidateUserDTO dto) throws AdminException;
 
-//	public Admin updateAdminDetails(Admin admin) throws AdminException;
+	public Admin updateAdminDetails(Admin admin) throws AdminException;
 	
-//	public Admin updateAdminPassword(UpdateUserPasswordDTO dto) throws AdminException;
+	public Admin updateAdminPassword(UpdateUserPasswordDTO dto) throws AdminException;
 	
-//	public Admin updateAdminUsername(UpdateUserUsername dto) throws AdminException;
+	public Admin updateAdminUsername(UpdateUserUsernameDTO dto) throws AdminException;
 
-//	public Admin deleteAdmin(Integer adminId) throws AdminException;
+	public Admin deleteAdmin(Integer adminId) throws AdminException;
 //
 //	public List<Activity> getAllActivities(Integer customerId) throws ActivityException;
 //
