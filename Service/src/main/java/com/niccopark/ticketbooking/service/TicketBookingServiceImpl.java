659628ac9cb5f1100d45782349b51fa0672feb72
Shiv-96 <kshiv.dot@gmail.com>
2023-02-22 14:30:17 +0530

    Ticketservice interface and their implementation class

---
+++ Service/src/main/java/com/niccopark/ticketbooking/service/TicketBookingServiceImpl.java
@@ -0,0 +1,45 @@
+package com.niccopark.ticketbooking.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.niccopark.entity.Ticket;
+import com.niccopark.exceptions.TicketException;
+
+@Service
+public class TicketBookingServiceImpl implements TicketBookingService {
+
+	@Override
+	public Ticket insertTicket(Ticket ticket) throws TicketException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Ticket updateTicket(Ticket ticket) throws TicketException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Ticket deleteTicket(Integer ticketId) throws TicketException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Ticket> viewAllTicketsCustomer(Integer customerId) throws TicketException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double calculateBill(Integer customerId) throws TicketException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
