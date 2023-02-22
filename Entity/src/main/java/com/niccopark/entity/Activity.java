a2ed0285c3875cdd8be438849bbbcaef2e652c8b
kanhaiya1007 <92618657+kanhaiya1007@users.noreply.github.com>
2023-02-22 04:56:50 -0800

    add

--- Entity/src/main/java/com/niccopark/entity/Activity.java
+++ Entity/src/main/java/com/niccopark/entity/Activity.java
@@ -1,9 +1,13 @@
 package com.niccopark.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,8 +26,8 @@ public class Activity {
 	
 	private Float charges;
 	
-//	@OneToMany(cascade = CascadeType.ALL)
-//	private List<Ticket> tickets;
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Ticket> tickets;
 	
 //	private String name;
 	
