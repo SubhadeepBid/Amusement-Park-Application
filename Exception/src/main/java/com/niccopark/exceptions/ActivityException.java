eb5aed91a991c829dca5b05ab2e747a7db778e13
SubhadeepBid <subhadipbid@gmail.com>
2023-02-22 10:59:35 +0530

    Exception Module updates

--- Exception/src/main/java/com/niccopark/exceptions/ActivityException.java
+++ Exception/src/main/java/com/niccopark/exceptions/ActivityException.java
@@ -1,15 +1,13 @@
 package com.niccopark.exceptions;
 
-public class ActivityException extends RuntimeException  {
+public class ActivityException extends RuntimeException {
 
 	public ActivityException() {
-		super();
 		// TODO Auto-generated constructor stub
 	}
 
 	public ActivityException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 }
