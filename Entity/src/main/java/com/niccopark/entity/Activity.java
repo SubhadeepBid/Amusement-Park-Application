fe6103f0dda2d03d6d986a444b206f4657e74ac8
Shiv-96 <kshiv.dot@gmail.com>
2023-02-24 17:21:20 +0530

    Validation of the entity classes and their dto classes

--- Entity/src/main/java/com/niccopark/entity/Activity.java
+++ Entity/src/main/java/com/niccopark/entity/Activity.java
@@ -3,6 +3,8 @@ package com.niccopark.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
@@ -15,6 +17,10 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,11 +34,17 @@ public class Activity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer activityId;
-
+	
+	@NotBlank(message = "Name can't be blank")
+	@NotNull(message = "Name can't be null")
 	private String name;
 
+	@NotBlank(message = "Descriptiona can't be blank")
+	@NotNull(message = "Descriptiona can't be null")
 	private String description;
 
+	@DecimalMin(value = "100.00", message = "Charges can't be less than 100")
+	@NotNull(message = "charges can't be null")
 	private Float charges;
 
 	// Apply @JsonIgnore here to avoid StackOverflow error
